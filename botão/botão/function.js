574be383ca7af5632e556d8248c650d73fb56091
Amanda Kaori <125595801+projamandakaori@users.noreply.github.com>
2024-10-06 20:32:39 -0300

    03/05 - questionário c/ botão

---
+++ botão/botão/function.js
@@ -0,0 +1,16 @@
+function btnpar(){
+    var x = 1024;
+    if(x%2 !==0){
+        alert("Ímpar");
+    }else{
+        alert("Par");
+    }
+}
+
+function btncorreto(){
+    alert("Correto!");
+}
+
+function btnincorreto(){
+    alert("Incorreto");
+}
