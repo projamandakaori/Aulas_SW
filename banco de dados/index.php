cbbb013f2a740034edea3a63091172c18e6699ae
Amanda Kaori <125595801+projamandakaori@users.noreply.github.com>
2024-11-11 21:40:56 -0300

    29/10 - Banco de dados

---
+++ banco de dados/index.php
@@ -0,0 +1,18 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="javascript.js"></script>
+
+<html>
+	<head>	
+		<input type="text" id="nome" value="">	
+		<br>
+		<button id="btConsulta">Consultar</button>
+	</head>
+</html>
+
+
+
+
+
+
+
